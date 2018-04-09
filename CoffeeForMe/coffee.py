5210e93f9b3ad438c43853779a880faefe138d22
Alex Bogdanovich <bogdanovich.alex@gmail.com>
2018-04-09 09:14:30 +0300

    daily push

--- CoffeeForMe/coffee.py
+++ CoffeeForMe/coffee.py
@@ -10,24 +10,24 @@ import sqlite3
 
 class coffeeTeam(object):
 	"""Base class"""	
-	def __init__(self, name=None, role=None, db="db.db"):
+	def __init__(self, name=None, role=None, password=None, db="db.db"):
 		"""save team member with his name and company role into db"""
 		
 		self.name = name
 		self.role = role
+		self.password = password
 		self.db = db
 		if (name is not None):
 			self.add_member()
 		
-	def get_members(self):
+	def get_member(self, name=None, password=None):
 		"""get the list of all team members"""
 		
 		connection = sqlite3.connect(self.db)
 		with connection:
-			result = connection.execute("SELECT * FROM team")
-			data = result.fetchall()
-			#print "added a new member: id {}".format(data[0])
-			return data
+			result = connection.execute("SELECT * FROM team WHERE name=? AND password=?", (name, password))
+			data = result.fetchone()
+			return len(data)
 			
 		
 	def remove_member(self):
@@ -40,12 +40,13 @@ class coffeeTeam(object):
 			connection.execute('''CREATE TABLE IF NOT EXISTS team (
 							   id INTEGER PRIMARY KEY AUTOINCREMENT,
 							   name TEXT,
+							   password TEXT,
 							   role TEXT
 							   )''')
 			
 			if (self.role is None):
 				self.role = 'unknown'
-			connection.execute("INSERT INTO team (name, role) VALUES (?,?)", (self.name, self.role))
+			connection.execute("INSERT INTO team (name, password, role) VALUES (?,?,?)", (self.name, self.password, self.role))
 			result = connection.execute("SELECT last_insert_rowid()")
 			data = result.fetchone()
 			print "added a new member: id {}".format(data[0])
@@ -64,6 +65,9 @@ class coffeeManager(coffeeTeam):
 			result = connection.execute("SELECT * FROM team WHERE role=?",(role,))
 			data = result.fetchall()
 			return data
+	def add_drink(self):
+		"""add a new drink into db"""
+		pass
 	
 class coffeeBarista(coffeeTeam):
 	"""Manager class with appropriate functions"""
@@ -84,3 +88,49 @@ class coffeeBarista(coffeeTeam):
 			result = connection.execute("SELECT * FROM team WHERE role=?",(role,))
 			data = result.fetchall()
 			return data
+		
+		
+		
+class coffeeDrink(object):
+	"""Base class of all drinks"""	
+	def __init__(self, name=None, options=None, price=None, db="db.db"):
+		"""save drink into db"""
+		
+		self.name = name
+		self.options = role
+		self.price = role
+		self.db = db
+		if (name is not None):
+			self.add_member()
+		
+	def get_drinks(self):
+		"""get the list of all coffee drinks"""
+		
+		connection = sqlite3.connect(self.db)
+		with connection:
+			result = connection.execute("SELECT * FROM team")
+			data = result.fetchall()
+			#print "added a new member: id {}".format(data[0])
+			return data
+			
+		
+	def remove_member(self):
+		"""remove employee from DB"""
+		
+		pass
+	def add_member(self):
+		connection = sqlite3.connect(self.db)
+		with connection:
+			connection.execute('''CREATE TABLE IF NOT EXISTS drink (
+							   id INTEGER PRIMARY KEY AUTOINCREMENT,
+							   name TEXT,
+							   options TEXT,
+							   price REAL
+							   )''')
+			
+			if (self.role is None):
+				self.role = 'unknown'
+			connection.execute("INSERT INTO drink (name, options, price) VALUES (?,?)", (self.name, self.options, self.price))
+			result = connection.execute("SELECT last_insert_rowid()")
+			data = result.fetchone()
+			print "added a new member: id {}".format(data[0])		
--- CoffeeForMe/main.py
+++ CoffeeForMe/main.py
@@ -8,32 +8,82 @@
 from coffee import coffeeTeam, coffeeManager, coffeeBarista
 import sys
 
-
-
-
 if __name__ == '__main__':
-	#for param in sys.argv:
-	#	print param
-	
-	if len(sys.argv) <= 1:
-		print "Please type `cofee.py -h` for more details"
+	if (len(sys.argv) <= 1) or (sys.argv[1] == "-h"):
+		print """
+		____________________________________________________
+		CofeeForMe v1.0 written by Alex Bogdanovich
+
+		Script Usage: cofee.py [option] [-L output log gile]
+		Options and arguments without login options:
+
+		-i		: interactive mode
 		
+		-a 		: create a new coffee team member (coffee.py -a manager Alex passwd)
+		-u		: login under specific user (coffee.py -u Alex passwd)
+		____________________________________________________
+		Options and arguments under logged user:
+		-r		: view revenue report
+		-c		: save barista's order 
+		____________________________________________________
+		"""
 	else:
-		
-		if (param[2] == "-h"):
-			print """
-			CofeeForMe v1.0 written by Alex Bogdanovich
-			Make your life better :)
-			---
-			Script Usage: cofee.py [option] [-L output log gile]
-			Options and arguments:
-			-h	: help script
-			-m	: create a new coffee manager
-			-b	: create a new coffee barman
-			-d	: creare a new drink with options
-			"""
-	#team = coffeeTeam()
-	#manager = coffeeManager()
-	#barista = coffeeBarista()
+		if (sys.argv[1] == "-a"):
+			"""add a new team member"""
+			if (sys.argv[2] == "manager"):
+				# coffe.py -a manager Alex pass123
+				manager = coffeeManager(sys.argv[3],sys.argv[2], sys.argv[4])
+			elif (sys.argv[2] == "barista"):
+				# coffe.py -a barista Alex pass123
+				barista = coffeeBarista(sys.argv[3],sys.argv[2], sys.argv[4])
+			else:
+				print "wrong team member role"
+		elif (sys.argv[1] == "-d"):
+			"""add a new drink into db"""
+			pass
+			
+		if (sys.argv[1] == "-i"):
+			"""interactive mode"""
+			while True:
+				print """
+				1 - Add a new CoffeForMe team member
+				2 - Login
+				0 - Exit
+				"""
+				selection = raw_input("Choose the menu item below: ")
+				if (not selection) or (selection == "q"): break
+				elif (selection == "1"):
+					"""add a new coffee team member"""
+					new_member = []
+					user_input = raw_input("Let's add a new CoffeeForMe team member: [manager|barista]? ")
+					if (user_input == "manager"):
+						# ask all what we need
+						new_member.append(user_input)
+						user_input = raw_input("Please enter user name and password ")
+						user_input = user_input.split()
+						new_member.append(user_input[0])
+						new_member.append(user_input[1])
+						# create a new team member
+						barista = coffeeTeam(new_member[1],new_member[0],new_member[2])
+					elif (user_input == "barista"):
+						# ask all what we need
+						new_member.append(user_input)
+						user_input = raw_input("Please enter user name and password ")
+						user_input = user_input.split()
+						new_member.append(user_input[0])
+						new_member.append(user_input[1])
+						# create a new team member
+						manager = coffeeTeam(new_member[1],new_member[0],new_member[2])
+				elif (selection == "2"):
+					# try to login
+					user_input = raw_input("Please enter user name and password ")
+					user_input = user_input.split()
+					member = coffeeTeam()
+					res = member.get_member(user_input[0], user_input[1])
+					print res #returned 4 ????
+				elif (selection == "0"):
+					break
+
+				
 	
 	
