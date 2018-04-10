6904e3d8d9953696d9e90c87bcb58a3a9e84b0f3
Alex Bogdanovich <bogdanovich.alex@gmail.com>
2018-04-10 14:22:17 +0300

    updated bash script

--- setup.sh
+++ setup.sh
@@ -5,16 +5,17 @@ cd ~/
 mkdir abogdanovich_test
 cd ~/abogdanovich_test
 virtualenv --no-site-packages coffee_project
-source coffee_project/bin/activate
 cd ~/abogdanovich_test/coffee_project/
-git clone https://github.com/abogdanovich/CoffeeForMe.git
-~/abogdanovich_test/coffee_project/bin/pip install -r CoffeeForMe/requirements.txt 
-~/abogdanovich_test/coffee_project/bin/python CoffeeForMe/src/main.py -h
+source bin/activate
 
+git clone https://github.com/abogdanovich/CoffeeForMe.git
+cd ~/abogdanovich_test/coffee_project/CoffeeForMe
+~/abogdanovich_test/coffee_project/bin/pip install -r requirements.txt 
 # run pytest
 cd ~/abogdanovich_test/coffee_project/CoffeeForMe/src
 pytest ~/abogdanovich_test/coffee_project/CoffeeForMe/src/test/test_coffeeTeam.py
 pytest ~/abogdanovich_test/coffee_project/CoffeeForMe/src/test/test_coffeeBarista.py
 pytest ~/abogdanovich_test/coffee_project/CoffeeForMe/src/test/test_coffeeManager.py
-
+# run main script
+~/abogdanovich_test/coffee_project/bin/python main.py -h
 
